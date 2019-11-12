afa5cbd1ee7dcf29559f35571b148094de321b16
fmarquesg <49692483+fmarquesg@users.noreply.github.com>
2019-11-12 11:25:37 -0300

    Update Veiculo.cs

--- classes/Veiculo.cs
+++ classes/Veiculo.cs
@@ -2,12 +2,103 @@
 
 public abstract class Veiculo
 {
+        #region Atributos
+
     protected string placa;
+
     protected Estacionada[] usos;
 
-	public Veiculo()
-	{
-	}
+    #endregion
 
-    public abstract double tarifa(Estacionada quando);
+    #region GetSets
+
+    public string getPlaca()
+    {
+
+        return this.placa;
+
+    }
+
+    public void setPlaca(string placa)
+    {
+
+        try
+        {
+            if (placa != "" && placa.Length == 7)
+            {
+
+                placa = placa;
+
+            }
+
+
+
+        }
+
+        catch (FormatException)
+        {
+
+            Console.WriteLine("O formato dos dados da placa está incorreto");
+
+        }
+
+        catch (ArgumentNullException)
+
+        {
+
+            Console.WriteLine("A string passada como valor da placa está vazia!");
+
+        }
+
+
+    }
+
+    public Estacionada getUsos(int posicao)
+    {
+
+        return this.usos[posicao];
+
+    }
+
+    public void setUsos(int posicao, Estacionada valorVaga)
+    {
+        try
+        {
+            if (this.usos[posicao] == null)
+            {
+
+                this.usos[posicao] = valor;
+
+            }
+
+        }
+
+        catch (FormatException)
+        {
+
+            Console.WriteLine("O formato de algum dos parâmetros não é válido!");
+
+        }
+
+        catch (ArgumentException)
+        {
+
+            Console.WriteLine("Algum dos parâmetros não é válido!");
+
+        }
+
+    }
+    #endregion
+
+    #region Métodos
+
+    public double tarifa(Estacionada quando)
+    {
+
+
+
+    }
+
+
+    #endregion
 }
