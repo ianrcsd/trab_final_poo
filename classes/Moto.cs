cf4f5cb09775d5dc350e405de81b517e0c9698b8
fmarquesg <49692483+fmarquesg@users.noreply.github.com>
2019-11-12 11:24:56 -0300

    Update Moto.cs

--- classes/Moto.cs
+++ classes/Moto.cs
@@ -2,11 +2,106 @@
 
 public class Moto
 {
-	public Moto()
+	    #region Atributos
+    public Moto()
 	{
-	}
 
-    public double tarifa(Estacionada quando)  {
-		return 0;
-	}
+        double desconto;
+
+        const double tarifaBasica = 5;
+
+    }
+
+    #endregion
+
+    #region GetSets
+    public double getTarifaBasica()
+    {
+
+        return tarifaBasica;
+
+    }
+
+    public double getDesconto()
+    {
+
+        return desconto;
+
+    }
+
+    public void setDesconto(double valor, double tarifaBasica)
+    {
+
+        if (valor > 0 && valor < tarifaBasica)
+
+            try
+            {
+
+                this.desconto = valor;
+
+            }
+
+            catch (ArgumentNullException)
+
+            {
+
+                Console.WriteLine("O parâmetro está nulo!");
+
+            }
+
+
+            catch (FormatException)
+
+            {
+
+                Console.WriteLine("Oformato do parâmetro está incorreto!");
+
+            }
+
+
+            catch (ArgumentException)
+
+            {
+
+                Console.WriteLine("O parâmetro não é válido!");
+
+            }
+
+    }
+
+
+    #endregion
+
+    
+    #region Métodos
+    public double tarifa(Estacionada quando) 
+    
+    {
+
+        double valorDesconto = 2;
+
+        double tarifaFinal = 0;
+
+        int tarifaHora = 5;
+
+        TimeSpan tempoEstacionada = Estacionada.saida.Subtract(Estacionada.entrada);
+
+        tarifaFinal = (tarifaHora - getDesconto()) * tempoEstacionada.Hours;
+
+        return tarifaFinal;
+
+    }
+
+    #endregion
+
+    #region Construtores
+
+    public Moto(string placa)
+    {
+
+        setPlaca(placa);
+
+    }
+
+    #endregion 
 }
