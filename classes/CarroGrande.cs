33a67cfbf9f1ea7f15875177887b59a127aeb644
fmarquesg <49692483+fmarquesg@users.noreply.github.com>
2019-11-12 11:23:02 -0300

    Update CarroGrande.cs

--- classes/CarroGrande.cs
+++ classes/CarroGrande.cs
@@ -2,11 +2,110 @@
 
 public class CarroGrande
 {
-	public CarroGrande()
-	{
-	}
+	 #region Atributos
 
-    public double tarifa(Estacionada quando)  {
-		return 0;
-	}
+
+    double valorExtra = 2;
+
+    const double tarifaBasica = 5;
+
+
+    #endregion
+
+    #region GetSets
+
+    public double getTarifaBasica()
+    {
+
+        return tarifaBasica;
+
+    }
+
+    public double getValorExtra()
+    {
+
+        return valorExtra;
+
+    }
+
+    public void setValorExtra(double valor)
+    {
+
+        if (valor > 0)
+
+            try
+            {
+
+                this.valorExtra = valor;
+
+            }
+
+            catch (ArgumentNullException)
+
+            {
+
+                Console.WriteLine("O parâmetro está nulo!");
+
+            }
+
+
+            catch (FormatException)
+
+            {
+
+                Console.WriteLine("Oformato do parâmetro está incorreto!");
+
+            }
+
+
+            catch (ArgumentException)
+
+            {
+
+                Console.WriteLine("O parâmetro não é válido!");
+
+            }
+
+    }
+
+
+    #endregion
+
+
+    #region Métodos
+    public double tarifa(Estacionada quando)
+    {
+
+        double tarifa(Estacionada quando)
+
+        {
+
+
+            double tarifaFinal = 0;
+
+            TimeSpan tempoEstacionada = Estacionada.saida.Subtract(Estacionada.entrada);
+
+            tarifaFinal = (getTarifaBasica() + getValorExtra()) * tempoEstacionada.Hours;
+
+            return tarifaFinal;
+
+
+        }
+
+
+
+    }
+
+    #endregion
+
+    #region Construtores
+
+    public CarroGrande(string placa)
+    {
+
+        setPlaca(placa);
+
+    }
+    #endregion
+
 }
