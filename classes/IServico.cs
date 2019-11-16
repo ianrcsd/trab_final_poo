8051c87e7af926d29670006d5d651151e90862b2
ianrcsd <54161291+ianrcsd@users.noreply.github.com>
2019-11-16 18:53:15 -0300

    Update IServico.cs

--- classes/IServico.cs
+++ classes/IServico.cs
@@ -1,6 +1,14 @@
 using System;
 
-interface IServico {
+public interface IServico
+{
 
+    #region Public methods
+    /// <summary>
+    /// Método para calcular o valor do serviço
+    /// </summary>
+    /// <returns>Retorna o valor dos serviços</returns>
     double valor();
+    #endregion
+
 }
